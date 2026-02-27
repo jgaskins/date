135945047d12865971c327bbbfd74e6d4d8ea8e7
Jamie Gaskins <jgaskins@hey.com>
2026-02-26 22:17:38 -0500

    Add support for Postgres `date` columns

---
+++ spec/pg_spec.cr
@@ -0,0 +1,30 @@
+require "./spec_helper"
+require "../src/pg"
+
+db = DB.open("postgres:///")
+
+describe Date do
+  it "parses Postgres `date` types" do
+    date = db.query_one("select '2026-02-26'::date", as: Date)
+    date.should eq Date.new(2026, 2, 26)
+  end
+
+  it "parses inside a DB::Serializable" do
+    user = db.query_one(<<-SQL, as: User)
+      SELECT
+        gen_random_uuid() id,
+        'Jamie' name,
+        '2026-02-26'::date created_on
+    SQL
+
+    user.created_on.should eq Date.new(2026, 2, 26)
+  end
+end
+
+struct User
+  include DB::Serializable
+
+  getter id : UUID
+  getter name : String
+  getter created_on : Date
+end
---
+++ src/pg.cr
@@ -0,0 +1,14 @@
+require "pg"
+
+struct Date
+  def self.new(rs : DB::ResultSet)
+    # The `pg` gem parses Postgres `date` columns as Crystal `Time` values
+    new rs.read(Time)
+  end
+end
+
+class PG::ResultSet
+  def read(date : Date.class)
+    Date.new self
+  end
+end
